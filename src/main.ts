576842f64f94471a554375b5114ec5420a346868
ハウル <haurjia@gmail.com>
2024-03-27 18:50:52 +0900

    fixed
    fixed

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { registerPlugins } from './plugins';
 import { router } from './router';
 import "./assets/css/cerceis.css"
 import '@fortawesome/fontawesome-free/css/all.css'
-import icon from './components/global/icon.vue';
+import icon from './components/global/Icon.vue';
 
 const app = createApp(App);
 app.component("fi", icon);
